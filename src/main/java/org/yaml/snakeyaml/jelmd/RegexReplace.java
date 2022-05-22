adb1cc512478c2f121f6fdc1512bbbf5d0bebb2a
Jens Elkner <jel+snakeyaml@cs.uni-magdeburg.de>
2022-05-22 16:54:56 +0200

    add Regex invert support for snmp_generator

--- src/main/java/org/yaml/snakeyaml/jelmd/RegexReplace.java
+++ src/main/java/org/yaml/snakeyaml/jelmd/RegexReplace.java
@@ -4,10 +4,13 @@ public class RegexReplace extends YamlObject {
 	public String regex;
 	public String value;
 	public String sub_oids;
+	public Boolean invert;
 
 	public void dump(StringBuilder sb, String indent) {
 		if (regex != null)
 			sb.append(indent).append("regex: '").append(regex).append("'\n");
+		if (regex != null)
+			sb.append(indent).append("invert: ").append(invert).append("\n");
 		if (sub_oids != null)
 			sb.append(indent).append("sub_oids: '").append(sub_oids).append("'\n");
 		if (value != null)
